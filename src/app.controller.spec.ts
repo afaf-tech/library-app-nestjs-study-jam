6a7a9123a95456c5b6c31bc980042f3d43cb945c
afaf-tech <fatihalfikri5@gmail.com>
2022-03-22 06:57:47 +0700

    unit test example

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -4,19 +4,26 @@ import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  // let appService: AppService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    // appService = new AppService();
+    // appController = new AppController(appService);
   });
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
+    });
+
+    it('shouldn\'t return "Hello World!"', () => {
+      expect(appController.notGetHello()).not.toBe('Hello World!');
     });
   });
 });
